a9209468ba80510cdf5efa6d538796bf69a3576c
Charlamada <pleasebitt@gmail.com>
2025-09-18 10:50:16 -0400

    Created commands

---
+++ hw0.2.go
@@ -0,0 +1,12 @@
+//Homework 0
+// Members Kimson, Nathan, Kevin
+// Q2 
+/* Write a program to implement a stack of integers using an array of size 100. 
+You can assume that your stack can never have 100 integers in the given workload. 
+Your stack only needs to provide push and pop.
+You should also implement a simple program to test your implementation (i.e., a series of pop and push).
+*/
+
+// 
+//
+//
