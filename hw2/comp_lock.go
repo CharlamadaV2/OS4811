476941a68a23d561d16924bc00bc6cffab650de4
CharlamadaV2 <pleasebitt@gmail.com>
2025-10-03 12:01:48 -0400

    created hw2 repo

---
+++ hw2/comp_lock.go
@@ -0,0 +1,107 @@
+// Ideally, it should alternate between two different lock implementations
+// and a standard sync.Mutex to compare their performance under contention.
+// This code benchmarks TicketLock and CASSpinLock implementations.
+
+package main
+
+import (
+	"fmt"     // Used for printing benchmark results
+	"runtime" // Used for Gosched() to yield in spin loops
+	"sync"    // Used to implement the locks
+	"sync/atomic" // Compare and Swap operations, atomic operations
+	"time"    // Used for measuring time
+)
+
+// TicketLock implements a ticket lock
+type TicketLock struct {
+	nowServing uint64 // Turn
+	nextTicket uint64 // Ticket
+}
+
+func (tl *TicketLock) Lock() {
+	myTicket := atomic.AddUint64(&tl.nextTicket, 1) - 1
+	for atomic.LoadUint64(&tl.nowServing) != myTicket {
+		runtime.Gosched() // Yield to other goroutines
+	}
+}
+
+func (tl *TicketLock) Unlock() {
+	atomic.AddUint64(&tl.nowServing, 1)
+}
+
+// CASSpinLock implements a compare-and-swap spin lock
+type CASSpinLock struct {
+	locked uint32
+}
+
+func (cas *CASSpinLock) Lock() {
+	for !atomic.CompareAndSwapUint32(&cas.locked, 0, 1) {
+		runtime.Gosched() // Yield to other goroutines
+	}
+}
+
+func (cas *CASSpinLock) Unlock() {
+	atomic.StoreUint32(&cas.locked, 0)
+}
+
+
+func benchmarkLock(name string, lock interface{}, numGoroutines int, iterations int) float64 {
+	var wg sync.WaitGroup
+	start := time.Now()
+	counter := int64(0) // Non-atomic, protected by lock
+	
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				switch l := lock.(type) {
+				case *TicketLock:
+					l.Lock()
+					counter++ // Simple increment
+					// Simulate tiny bit of work
+					_ = counter * 2
+					l.Unlock()
+					
+				case *CASSpinLock:
+					l.Lock()
+					counter++
+					_ = counter * 2
+					l.Unlock()
+					
+				case *sync.Mutex:
+					l.Lock()
+					counter++
+					_ = counter * 2
+					l.Unlock()
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+	duration := time.Since(start).Seconds()
+	throughput := float64(numGoroutines*iterations) / duration
+	fmt.Printf("%s (G: %d, Iter: %d): %.4fs, %.0f ops/sec, %.2f ns/op\n",
+		name, numGoroutines, iterations, duration, throughput, 
+		duration*1e9/float64(numGoroutines*iterations))
+	return duration * 1e9 / float64(numGoroutines*iterations)
+}
+
+func main() {
+	// Test configurations
+	goroutineCounts := []int{1, 10, 50, 100}
+	iterations := 10000
+
+	for _, g := range goroutineCounts {
+		fmt.Printf("\nTesting with %d goroutines\n", g)
+		
+		// Ticket Lock
+		ticketLock := &TicketLock{}
+		benchmarkLock("TicketLock", ticketLock, g, iterations)
+		
+		// CAS Spin Lock
+		casLock := &CASSpinLock{}
+		benchmarkLock("CASSpinLock", casLock, g, iterations)
+	}
+}
