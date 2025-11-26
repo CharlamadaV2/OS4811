af92a5fe5fa1d6f6a724327ec1ac2cce0297a372
Charlamada <pleasebitt@gmail.com>
2025-11-26 16:01:28 -0700

    checkpoint for test

--- knkfs/functions/test.c
+++ knkfs/functions/test.c
@@ -6,6 +6,7 @@
 #include <linux/spi/spidev.h> // functions for SPI devices (for EEPROM)
 #include <stdint.h>
 #include <stdio.h>
+#include <errno.h>  // For errno
 
 #define num_blocks 16
 
@@ -23,18 +24,34 @@ typedef struct {
     extended_inode xtd_inode[num_blocks];
 } extended_inode;
 
-typedef struct textfile_properties {
+typedef struct {
     //
 
 } text_file; 
 
-void write(char name[12], uint8_t type, uint16_t address){
+typedef struct {
+    //
+
+} config_file; 
+
+
+/* Store all meta data onto the EEPROM */
+
+void create_file(char name[12], uint8_t type, uint16_t address){
 //File name, File type, File address (offset)
+
+//Check if file type with name already exists
+//Ask if write wants to overwrite
+//If yes remove inode pointer, update blockmap vacancy, update table
+//If no exit from prompt
+
     inode new_file;
     switch (type) {
+        //For the test only txt and config will be created
         case 1: 
             printf("Creating txt file");
             new_file.type = 1;
+            create_txt();
             break;
         case 2: 
             printf("Creating config file");
@@ -48,12 +65,33 @@ void write(char name[12], uint8_t type, uint16_t address){
         printf("This file type does not exist...");
     }
 
+}
+
+void read_file(char name[12], uint8_t type){
+    //Check if file exist with name and type
+    // If not; error message
+    // Exit program
+    // If exists; read value inside file
+    // Print out file properties
+    // Exit program
+}
+
+void list_files(){ //Read files inside Inode Table
+
+
 
 
 }
 
-void create_txt(){
-    //Update inode based on what is written
+void write(){ //This function will contain SPI processes
+    
+    
+
+
+}
+
+void read(){ //This function will contain SPI processes
+
 
 
 
